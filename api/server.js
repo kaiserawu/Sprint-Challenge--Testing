c69bd7c5c7331f98845265d77a2fc808edf1d7af
kaiserawu <kaiser.a.wu@gmail.com>
2019-06-29 11:37:25 -0700

    Implement GET endpoints, all tests pass, mvp complete

--- api/server.js
+++ api/server.js
@@ -56,4 +56,12 @@ server.post('/games', async (req, res) => {
   }
 })
 
+server.get('/games', async (req, res) => {
+  try {
+    res.status(200).json(server.games);
+  } catch(err) {
+    res.status(500).json({ error: err });
+  }
+})
+
 module.exports = server;
